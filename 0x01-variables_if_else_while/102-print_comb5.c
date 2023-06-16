cf934097eb531bcf403556f7a32658cea3154c0d
Rheedn <algoy6jacob@gmail.com>
2023-06-17 00:15:55 +0100

    Sleepy

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,7 +17,7 @@ int main(void)
 			putchar(' ');
 			putchar((j / 10) + 48);
 			putchar((j % 10) + 48);
-			if (i != 98 || j != 99)
+			if (i != 98 || j != 98)
 			{
 				putchar(',');
 				putchar(' ');
