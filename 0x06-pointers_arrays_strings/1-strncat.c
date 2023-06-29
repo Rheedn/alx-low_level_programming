d5dd635daa8e41be832311e54c91db32754c093c
Rheedn <algoy6jacob@gmail.com>
2023-06-29 22:45:27 +0100

    Array n some

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,24 +1,23 @@
 #include "main.h"
-
 /**
-  *_strncat - concatenates two strings
-  *using at most n bytes from src
-  *@dest: input value destination
-  @src: input value source
-  *@n: input value bytes
-  *
-  *Return: dest
-  */
-char *_strncat(char *dest, char *src, int n);
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
 
-	i = 0
-		while (dest[i] != '\0')
-		{
-			i++;
-		}
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
 	j = 0;
 	while (j < n && src[j] != '\0')
 	{
@@ -29,3 +28,4 @@ char *_strncat(char *dest, char *src, int n);
 	dest[i] = '\0';
 	return (dest);
 }
+
