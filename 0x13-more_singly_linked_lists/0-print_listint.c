ab463a9ff4d97a155732f79a43361ba97389d9eb
Rheedn <algoy6jacob@gmail.com>
2023-08-01 23:53:58 +0100

    lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all elements of  linked list
+ * @h: Pointer to head of the linked list
+ *
+ * Return: number of nodes in linked list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t count = 0;
+
+
+	while (h != NULL)
+	{
+		printf("%d ", h->n);
+		h = h->next;
+		count++;
+	}
+	printf("\n");
+
+	return (count);
+}
