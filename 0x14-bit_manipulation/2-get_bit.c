db54e482cd652c2cdc3e9efe51e8c2838cccf88c
Rheedn <algoy6jacob@gmail.com>
2023-08-03 23:59:43 +0100

    Declaration fault

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,7 +9,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int mask = 1UL << index;
+	unsigned long int mask;
+
+	mask = 1UL << index;
 
 	if (index >= (sizeof(unsigned long int) * 8))
 		return -1;
