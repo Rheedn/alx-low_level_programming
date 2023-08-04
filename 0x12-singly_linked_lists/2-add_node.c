c7523db115d10fb3cb644cd9d486c1f9ecbe9532
Rheedn <algoy6jacob@gmail.com>
2023-08-04 21:12:06 +0100

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -13,10 +13,12 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *new_node;
+
 	if (str == NULL)
 		return (NULL);
 
-	list_t *new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(list_t));
 
 	if (new_node == NULL)
 		return (NULL);
