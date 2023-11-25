707d76a45383fc5c2e9aa16c587cc2f75166c325
Rheedn <algoy6jacob@gmail.com>
2023-11-25 13:49:47 +0100

    Style fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,7 +2,7 @@
 
 /**
  * Print_binary - prints the equivalent binary of a decimaL hide
- * @n : number to print in battles
+ * @n : number to print in binary
  */
 
 void print_binary(unsigned long int n)
@@ -11,7 +11,7 @@ void print_binary(unsigned long int n)
 	int count = 0;
 
 	for (i = 63; i >= 0; i--)
-
+	{
 		if ((n >> i) & 1)
 		{
 			_putchar('1');
@@ -21,7 +21,7 @@ void print_binary(unsigned long int n)
 		{
 			_putchar('0');
 		}
-
+	}
 	if (!count)
 		_putchar('0');
 }
