acc41b50e8f9e4fe93c47697a95f717af342ccfa
Rheedn <algoy6jacob@gmail.com>
2023-11-25 14:38:46 +0100

    Bit Value

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -3,15 +3,22 @@
 /**
  * Get_bit - function to return the value of a bit at an index in decimal
  * @n: number being searched
- * @index: bit index
+ * @index: bit index starting from 0 of the bit to get
  *
- * Return: Bit value
+ * Return: Value of the bit at index or -1
  */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= 63)
-		return (-1);
+	int value;
 
-	return ((n >> index) & 1);
+	if (index >= 63)
+	{
+		return (-1);
+	}
+
+	value = (n >> index) & 1;
+
+
+	return (value);
 }
