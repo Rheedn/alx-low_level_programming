4a80c1b8ebc1bf39ff35c4929cce17a4eaae0360
Rheedn <algoy6jacob@gmail.com>
2023-11-25 13:43:40 +0100

    Print Binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * Print_binary - prints the equivalent binary of a decimaL hide
+ * @n : number to print in battles
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	int count = 0;
+
+	for (i = 63; i >= 0; i--)
+		
+		if ((n >> i) & 1)
+		{
+			_putchar('1');
+		count++;
+		}
+		else if (count)
+		{
+			_putchar('0');
+		}
+
+	if (!count)
+		_putchar('0');
+}
