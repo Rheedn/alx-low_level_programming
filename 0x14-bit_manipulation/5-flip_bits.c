94e2fb14cc16a88a1a714134df7626d2bab54e28
Rheedn <algoy6jacob@gmail.com>
2023-11-28 13:30:57 +0100

    0-main.c added

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits to change
+ * @n: 1st number 
+ * @m: 2nd number
+ *
+ * Return: The number of bits to change
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
+}
