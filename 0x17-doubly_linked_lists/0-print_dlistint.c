76ba5de91338ee4b4c0b681d5337883f58d7b31b
Algoy Jacob <algoy6jacob@gmail.com>
2024-01-11 22:46:49 +0100

    msh

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,20 +10,16 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count;
+	size_t count;
 
 	count = 0;
 
-	if (h == NULL)
-		return (count);
-
-	while (h->prev != NULL)
-		h = h->prev;
-
-	while (h != NULL)
+	while (h)
 	{
-		printf("%d\n", h->n);
 		count++;
+
+		printf("%d\n", h->n);
+
 		h = h->next;
 	}
 
