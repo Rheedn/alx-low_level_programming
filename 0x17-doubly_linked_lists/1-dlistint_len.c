3043361b9d782e7c47601b5592e37be2df06cea2
Algoy Jacob <algoy6jacob@gmail.com>
2024-01-11 22:56:17 +0100

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - counts number of elements in linked dlistint_list
+ * @h: the head of the lsit
+ *
+ * Return: Number of elements in the list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h)
+	{
+		count++;
+
+		h = h->count;
+	}
+
+	return (count)
+}
